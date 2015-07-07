e705bea1dbbad85fde1d5a54381256befbd3cf7f
Zacharias Steinmetz <steinmetz@posteo.de>
2015-07-07 20:05:56 +0200

    Optimized reading function
    Helper function tt2df omitted

--- R/read.R
+++ R/read.R
@@ -1,32 +1,3 @@
-#' @title Fixed-format tables
-#'
-#' @description
-#' Generic function to convert a fixed-format Fortran table into a \code{data.frame}.
-#' @usage
-#' tt2df(data, col.names, prefix, suffix)
-#'
-#' @param data Table body as vector of strings as read, e.g., by \code{readLines}
-#' @param col.names Column names as string separated by whitespaces
-#' @param prefix Optional data to insert before the data table
-#' @param suffix Optional data to insert after the data table
-#'
-#' @seealso
-#' \code{\link{read.wofost}}
-#'
-tt2df <- function(data, col.names, prefix, suffix) {
-  df <- data.frame(matrix(unlist(strsplit(data, "\\s+")),
-                          nrow=length(data), byrow=T))[,-1]
-  names(df) <- unlist(strsplit(col.names, "\\s+"))[-1]
-  for (i in names(df)) df[,i] <- type.convert(as.character(df[,i]))
-
-  if (missing(prefix) & missing(suffix)) out <- df
-  if (missing(prefix) & !missing(suffix)) out <- cbind(df, suffix)
-  if (!missing(prefix) & missing(suffix)) out <- cbind(prefix, df)
-  if (!missing(prefix) & !missing(suffix)) out <- cbind(prefix, df, suffix)
-
-  return(out)
-}
-
 #' @title Read WOFOST results
 #'
 #' @description
@@ -37,27 +8,28 @@ tt2df <- function(data, col.names, prefix, suffix) {
 #' @param file The name of the file which the data are to be read from.
 #' Valid extensions are: *.out, *.pps, or *.wps
 #'
-#' @seealso
-#' \code{\link{tt2df}}
+#' @return
+#' Returns a \code{list} when reading detailed crop outputs (*.out), otherwise a
+#' \code{data.frame} is generated.
 #'
 read.wofost <- function(file) {
   if (missing(file) | !grepl("\\.pps$|\\.wps$|\\.out$", file))
     stop("Specify WOFOST output file.")
 
   if (grepl("\\.pps$|\\.wps$", file)) {
-    txt <- readLines(file)
-    cols <- txt[grep("RUNNAM", txt)]
-    data <- txt[(grep("RUNNAM", txt)+1):length(txt)]
-    out <- tt2df(data, cols)
+    text <- readLines(file)
+    out <- read.table(textConnection(text),
+                      skip = grep("RUNNAM", text))
+    names(out) <- unlist(strsplit(text[grep("RUNNAM", text)], "\\s+"))[-1]
   }
 
   if (grepl("\\.out$", file)) {
     out <- list()
-    txt <- readLines(file)
-    run <- c(grep("RUNNAM", txt), length(txt)+1)
+    text <- readLines(file)
+    runs <- c(grep("RUNNAM", text), length(text)+1)
 
-    for (i in 2:length(run)) {
-      current <- txt[run[i-1]:(run[i]-1)]
+    for (i in 2:length(runs)) {
+      current <- text[runs[i-1]:(runs[i]-1)]
       name <- trimws(unlist(strsplit(current[1], "->"))[2])
       header <- c()
       for (head in c("WEATHER", "CROP", "SOIL")) {
@@ -66,15 +38,15 @@ read.wofost <- function(file) {
       start <- trimws(unlist(strsplit(grep("START", current, value=T), "->")))[2]
       dates <- unique(na.omit(as.numeric(unlist(strsplit(current[grep("START", current)+1], "[^0-9]+")))))
       dates[dates==99] <- NA
-
       type <- tolower(trimws(grep("CROP PRODUCTION", current, value = T)))
 
-      cols <- current[grep("YEAR", current)]
-      data <- current[(grep("YEAR", current)+3):(grep("SUMMARY", current)-2)]
-
-      df <- tt2df(data, cols,
-                  data.frame(RUNNAM=name, TYPE=type,
+      df <- read.table(text = current,
+                       skip = grep("YEAR", current)+2,
+                       nrows = grep("SUMMARY", current)-grep("YEAR", current)-4)
+      names(df) <- unlist(strsplit(current[grep("YEAR", current)], "\\s+"))[-1]
+      df <- cbind(data.frame(RUNNAM=name, TYPE=type,
                              ISDAY=dates[1], DOS=dates[2], DOE=dates[3]),
+                  df,
                   data.frame(WEATHER=header[1], CROP=header[2], SOIL=header[3],
                              START=start))
       out[[type]] <- rbind(out[[type]], df)
