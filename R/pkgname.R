da98497cd26bb9ceeffdec0705b1eaf87bf4b808
Zacharias Steinmetz <steinmetz@posteo.de>
2015-07-07 18:58:47 +0200

    Package info

---
+++ R/pkgname.R
@@ -0,0 +1,28 @@
+#' @title Handling WOFOST model files
+#'
+#' @description
+#' WOFOST (WOrld FOod STudies) is a simulation model for the quantitative
+#' analysis of the growth and production of annual field crops.
+#' This packages provides functions to handle WOFOST parameter input files and
+#' model results using \R{}.
+#'
+#' @details
+#' \tabular{ll}{
+#' Version: \tab 0.1, tested with WOFOST 7.1.7\cr
+#' Licence: \tab GPLv3\cr
+#' }
+#'
+#' @author Zacharias Steinmetz (\email{stei4785@@uni-landau.de})
+#'
+#' @references
+#' Boogaard HL, De Wit AJW, te Roller JA, Van Diepen CA, 2014. User's guide for
+#' the WOFOST Control Centre 2.1 and the crop growth simulation model WOFOST 7.1.7.
+#' Wageningen (Netherlands), Alterra, Wageningen University & Research
+#' Centre.
+#'
+#' @seealso
+#' \url{http://www.wageningenur.nl/en/Expertise-Services/Research-Institutes/alterra/Facilities-Products/Software-and-models/WOFOST.htm}
+#'
+#' @docType package
+#' @name wofostats
+NULL
